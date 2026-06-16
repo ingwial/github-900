6f63f406fd5e9c4befc62c99e42bea271ae1f3c1
Wilfredo Alvarez <162920279+ingwial@users.noreply.github.com>
2026-06-16 15:59:46 -0400

    Add hello_world.js with HTML and JavaScript example

---
+++ hello_world.js
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+<body>
+
+  <h1 id="greeting"></h1>
+
+  <script>
+    // Option A: Write directly to the screen
+    document.write("Hello, World!"); 
+
+    // Option B: Change the text of an HTML element
+    document.getElementById("greeting").innerText = "Hello, World!";
+  </script>
+
+</body>
+</html>
